936cda45bf534990f5137b01ee8469dc91eb9030
firdaushqq <firdaushaqiqi17@gmail.com>
2020-08-19 16:30:00 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe velit at et itaque impedit ratione dignissimos hic id, fugiat accusantium veritatis non voluptatibus, harum architecto obcaecati. Fugit eos voluptas dolores.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
