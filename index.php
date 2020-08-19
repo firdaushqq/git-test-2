0424efe7575126f4eae422574e18fd8888cb0f71
firdaushqq <firdaushaqiqi17@gmail.com>
2020-08-19 16:23:54 +0700

    Menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe velit at et itaque impedit ratione dignissimos hic id, fugiat accusantium veritatis non voluptatibus, harum architecto obcaecati. Fugit eos voluptas dolores.</p>
+</body>
+</html>
